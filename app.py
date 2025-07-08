90dbba7ee063ef325a163cb587242a5e967beb6b
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-08 15:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,20 +32,20 @@ elif st.session_state.page == "analyze":
     col1, col2, col3, col4, = st.columns(4)
     with col1:
         sel_fy = st.multiselect("Financial_Year", sorted(df["FY"].dropna().unique()))
-    if sel_fy:
-        fil_df=fil_df[fil_df["FY"].isin(sel_fy)]
+        if sel_fy:
+            fil_df=fil_df[fil_df["FY"].isin(sel_fy)]
     with col2:
         sel_td = st.multiselect("Time_Of_Day", sorted(df["Day / Night"].dropna().unique()))
-    if sel_td:
-        fil_df=fil_df[fil_df["Day / Night"].isin(sel_td)]
+        if sel_td:
+            fil_df=fil_df[fil_df["Day / Night"].isin(sel_td)]
     with col3:
         sel_so = st.multiselect("State_Office", sorted(df["SO"].dropna().unique()))
         if sel_so:
             fil_df=fil_df[fil_df["SO"].isin(sel_so)]
     with col4:
         sel_tt = st.multiselect("TT_Type", sorted(df["Type of TT "].dropna().unique()))
-    if sel_tt:
-        fil_df=fil_df[fil_df["Type of TT "].isin(sel_tt)]
+        if sel_tt:
+            fil_df=fil_df[fil_df["Type of TT "].isin(sel_tt)]
     tab1,tab2,=st.tabs(["Data","Chart"])
     with tab1:
             st.dataframe(fil_df)

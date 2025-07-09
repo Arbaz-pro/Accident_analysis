33985f20a3906f6896fc093374b259f11f53f9e7
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-09 09:44:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,16 +54,16 @@ elif st.session_state.page == "analyze":
         st.subheader("State Wise Distribution for Packed TT's")
 
         grouped = (
-                fil_df.groupby(["SO", "FY"])
-                .size()
-                .reset_index(name="Total Accidents")
-                )
+        fil_df.groupby(["SO", "FY"])
+        .size()
+        .reset_index(name="Total Accidents")
+        )
         color_palette = ["#1f77b4", "#4c72b0", "#6baed6", "#9ecae1", "#b2df8a", "#a6cee3", "#fdbf6f", "#c7e9c0", "#fb9a99", "#d9d9d9"]
         bar_fig = px.bar(
         grouped,
-        x="State Office",
+        x="SO",
         y="Total Accidents",
-        color="Financial Year",
+        color="FY",
         title="Total Accident distribution",
         text_auto=True,
         color_discrete_sequence=color_palette

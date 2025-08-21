93ba242b91c9ebad07c083163142e331d62baedd
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-08-21 15:10:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -205,13 +205,14 @@ elif st.session_state.page == "analyze":
 
             st.plotly_chart(fig, use_container_width=True)
         elif(sel_ch=="Injury & Fatality Overview"):
-            grouped_df = df.groupby("Financial_Year")[["Injury Others", "Fatality"]].sum().reset_index()
+            grouped_df = df.groupby("")[["Injury Others", "Fatality Others"]].sum().reset_index()
             fig = px.line(grouped_df, 
-              x="Financial_Year", 
-              y=["Injury Others", "Fatality"],
+              x="FY", 
+              y=["Injury Others", "Fatality Others"],
               markers=True,
               title="Trend of Injuries and Fatalities over Financial Years")
 
             st.plotly_chart(fig, use_container_width=True)
+
 
 
